6e2cb47348e297cdab891320b4e3aceb8350d11c
basam <basam.najwa11@gmail.com>
2025-03-26 20:17:20 +0400

    AssemblyEndgame coding

---
+++ src/AssemblyEndgame.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const AssemblyEndgame = () => {
+  return (
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the word within 8 attempts to keep the programming world safe
+          from Assemnly
+        </p>
+      </header>
+      <section className="game-status">
+        <h2>You win</h2>
+        <p>Well done!</p>
+      </section>
+    </main>
+  );
+};
+export default AssemblyEndgame;
