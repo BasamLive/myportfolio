108559ebba4d6810a960316bf53476b31c6f8870
basam <basam.najwa11@gmail.com>
2025-04-01 03:47:22 +0400

    removed the starter button

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
-import { useState } from "react";
-
 import AssemblyEndgame from "./AssemblyEndgame";
 import "./App.css";
 
 const App = () => {
-  const [startGame, setStartGame] = useState(false);
   const turnGameOn = () => setStartGame((c) => !c);
 
   return (
     <>
-      {startGame && <AssemblyEndgame />}
-      {!startGame && (
-        <button className="game-starter" onClick={turnGameOn}>
-          click me
-        </button>
-      )}
+      <AssemblyEndgame />
     </>
   );
 };
